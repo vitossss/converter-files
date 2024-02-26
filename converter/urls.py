c8fa0a83177b66197770012e063ea91e9a3d29e1
vitossss <shvab7216@gmail.com>
2024-02-26 23:42:46 +0200

    update logic for drf: - add serializer - update APIView - update convert logic - update dependencies

--- converter/urls.py
+++ converter/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("csv_convertor.urls")),
+    path('api/', include("csv_convertor.urls")),
 ]
